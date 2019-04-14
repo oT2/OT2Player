8041ee3fcce21e11c9235c0cb6f31f6a64f83cfd
unknown <gabriel.goude@outlook.com>
2019-04-14 16:47:29 +0200

    added example project

---
+++ VideoDecoder.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoDecoder.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var decoder = new Ot2Player.VideoDecoders.VideoDecoder(Ot2Player.VideoDecoders.Codec.H264, new Ot2Player.VideoDecoders.Resolution());
+        }
+    }
+}
